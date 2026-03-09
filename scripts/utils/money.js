3d29bb2caa8200b077eb409e530c4c5b8cc5c0d1
Rodrigo <2148722@student.uma.pt>
2026-03-09 14:42:28 +0000

    AndAddMoneyTests

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,9 +1,7 @@
 function formatCurrency(priceCents) {
-    return (Math.round(priceCents) / 100).toFixed(2);
-    // return (priceCents / 100).toFixed(2);
-    // return (Math.round(priceCents) / 100).toFixed(2);
-    // return (priceCents / 100).toFixed(2);
-
-
+  return (Math.round(priceCents) / 100).toFixed(2);
+  // return (priceCents / 100).toFixed(2);
+  // return (Math.round(priceCents) / 100).toFixed(2);
+  // return (priceCents / 100).toFixed(2);
 }
 export default formatCurrency;
